7de1729c2d8f1b9a8c238a1c3d9005226ef871a4
reza107-hub <rashidaminur104@gmail.com>
2023-05-19 12:02:01 +0600

    firebase installed and authprovider setup privateroute addeed

---
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const updateProf = (name, photo) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  const logInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logOut = () => {
+    signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
+  const authInfo = { user, loading, createUser,signIn,updateProf,logInWithGoogle,logOut };
+  return (
+    <div>
+      <div>
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+      </div>
+    </div>
+  );
+};
+
+export default AuthProvider;
---
+++ src/Components/Routes/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { RotatingLines } from "react-loader-spinner";
+import { AuthContext } from "../AuthProbider/AuthProvider";
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="mt-20">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="96"
+            visible={true}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
