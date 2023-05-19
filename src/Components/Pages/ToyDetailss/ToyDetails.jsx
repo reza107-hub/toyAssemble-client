69ecc1fdf5805602fd4de1e49b9cde24d9eeb36a
reza107-hub <rashidaminur104@gmail.com>
2023-05-19 22:21:41 +0600

    toy details done

--- src/Components/Pages/ToyDetailss/ToyDetails.jsx
+++ src/Components/Pages/ToyDetailss/ToyDetails.jsx
@@ -1,9 +1,48 @@
+import { useParams, useLoaderData } from "react-router-dom";
+
 const ToyDetails = () => {
-    return (
-        <div>
-            Toydetails
+  const { id } = useParams();
+  const toys = useLoaderData();
+  const toy = toys.find((item) => item.id == id);
+
+  const {
+    image,
+    name,
+    seller,
+    seller_email,
+    price,
+    rating,
+    quantity,
+    description,
+  } = toy;
+
+  return (
+    <div className="container mx-auto my-8">
+      <div className="flex flex-col md:flex-row gap-7 items-center md:justify-around">
+        <img src={image} alt={name} className="w-64 h-64 md:h-[500px] md:w-[50%] mb-4" />
+
+        <div className="md:w-[1200px]">
+          <h2 className="text-3xl font-bold text-primary mb-2">{name}</h2>
+          <p className="text-gray-600 mb-2">
+            <span className="font-bold ">Seller:</span> {seller}
+          </p>
+          <p className="text-gray-600 mb-4">
+            <span className="font-bold">Seller Email:</span> {seller_email}
+          </p>
+          <p className="text-lg mb-2 text-secondary">
+            <span className="font-bold">Price:</span> ${price}
+          </p>
+          <p className="text-lg mb-2 text-secondary">
+            <span className="font-bold">Rating:</span> {rating}/5
+          </p>
+          <p className="text-lg mb-2 text-secondary">
+            <span className="font-bold">Available Quantity:</span> {quantity}
+          </p>
+          <p className="text-gray-500 mb-4">{description}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ToyDetails;
--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -52,6 +52,7 @@ export const router = createBrowserRouter([
             <ToyDetails />
           </PrivateRoute>
         ),
+        loader: () => fetch("category.json"),
       },
     ],
   },
